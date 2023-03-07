5f34efe7243139561bb1a8150b9e2eb9c4d2384f
kshwi <kshwiss@gmail.com>
2023-03-06 22:18:31 -0600

    final troubleshooting for now

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 var Switcher = document.getElementsByClassName("Question");
 var timerEl = document.getElementById("timer");
 var StartButton = document.getElementById("Starter");
-var rightButtons = document.getElementsByClassName("right");
-var wrongButtons = document.getElementsByClassName("wrong");
-var Result = document.getElementsByClassName("result");
+var R1 = document.getElementById("right1");
+var R2 = document.getElementById("right2");
+var R3 = document.getElementById("right3");
+var W1 = document.getElementById("wrong1");
+var W2 = document.getElementById("wrong2");
+var W3 = document.getElementById("wrong3");
+var W4 = document.getElementById("wrong4");
+var W5 = document.getElementById("wrong5");
+var W6 = document.getElementById("wrong6");
+var W7 = document.getElementById("wrong7");
+var W8 = document.getElementById("wrong8");
+var Respite = document.getElementById("result");
 var DisplayHighScore = document.getElementById("highscores");
 
 var mostRecentScore = localStorage.getItem("posted-score");
@@ -16,11 +25,9 @@ var PostScore = document.getElementById("posted-score");
 var score = 0;
 var Name = document.getElementById("form1").value;
 
-//This countdown function doesn't happen
-StartButton.addEventListener("click", countdown());
-
+var timeLeft = 55;
 function countdown() {
-  var timeLeft = 55;
+  console.log("high");
 
   var timeInterval = setInterval(function() {
     if (timeLeft > 1) {
@@ -36,17 +43,63 @@ function countdown() {
       clearInterval(timeInterval);
     }
 
-    if (wrongButtons) {
-      timeLeft - 5;
-      Result.textContent = "Wrong!";
-      preventDefault();
-    } else if (rightButtons) {
-      Result.textContent = "Correct!";
-      score++;
-      preventDefault();
-    }
-
-    PostScore = score;
+    $("#posted-score").text(score);
   }, 1000);
 }
+var EndTimer = function() {
+  timeLeft = 0;
+};
+W1.addEventListener("click", function() {
+  timeLeft -= 5;
+  Respite.textContent = "Wrong!";
+});
+W2.addEventListener("click", function() {
+  timeLeft -= 5;
+  Respite.textContent = "Wrong!";
+});
+W3.addEventListener("click", function() {
+  timeLeft -= 5;
+  Respite.textContent = "Wrong!";
+});
+W4.addEventListener("click", function() {
+  timeLeft -= 5;
+  Respite.textContent = "Wrong!";
+});
+W5.addEventListener("click", function() {
+  timeLeft -= 5;
+  Respite.textContent = "Wrong!";
+});
+W6.addEventListener("click", function() {
+  timeLeft -= 5;
+  Respite.textContent = "Wrong!";
+  EndTimer();
+  $("#posted-score").text(score);
+});
+W7.addEventListener("click", function() {
+  timeLeft -= 5;
+  Respite.textContent = "Wrong!";
+  EndTimer();
+  $("#posted-score").text(score);
+});
+W8.addEventListener("click", function() {
+  timeLeft -= 5;
+  Respite.textContent = "Wrong!";
+  EndTimer();
+  $("#posted-score").text(score);
+});
+R1.addEventListener("click", function() {
+  Respite.textContent = "Correct!";
+  score++;
+});
+R2.addEventListener("click", function() {
+  Respite.textContent = "Correct!";
+  score++;
+});
+R3.addEventListener("click", function() {
+  Respite.textContent = "Correct!";
+  score++;
+  EndTimer();
+  $("#posted-score").text(score);
+});
+StartButton.addEventListener("click", countdown);
 localStorage.setItem("highscores", JSON.stringify(score));
